0b12046e7107b9b4d6a8b2d8b36fead0df1d74f8
Marcos Barranquero <marcos.barranquero@uah.es>
2022-11-20 18:06:44 +0100

    functional chat

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,20 +7,33 @@ const socket = io(`http://localhost:${BACKEND_PORT}/`)
 
 function App() {
   const [message, setMessage] = useState('')
+  const [messages, setMessages] = useState([
+    {
+      body: 'Welcome to the chat!',
+      from: 'Web',
+    },
+  ])
 
   useEffect(() => {
-    const receiveMessage = message => console.log(message)
+    const receiveMessage = message => {
+      setMessages([message, ...messages])
+    }
 
     socket.on('message', receiveMessage) // listen for message from server
 
     return () => socket.off('message') // unsubscribe
-  }, [])
+  }, [messages])
 
   const handleInput = e => setMessage(e.target.value) // updates message state
   const handleSubmit = e => {
     e.preventDefault() // prevent page reload
     socket.emit('message', message) // send message to server <key, value>
     setMessage('') // clear input
+    const newMessage = {
+      body: message,
+      from: 'Me',
+    }
+    setMessages([newMessage, ...messages]) // add message to messages state
   }
 
   return (
@@ -29,6 +42,14 @@ function App() {
         <input type='text' onChange={e => handleInput(e)} value={message} />
         <button>Send</button>
       </form>
+
+      {messages.map((message, index) => (
+        <div key={index}>
+          <p>
+            {message.from}: {message.body}
+          </p>
+        </div>
+      ))}
     </div>
   )
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -22,7 +22,10 @@ socketioServer.on('connection', socket => {
   console.log('New client connected')
   socket.on('disconnect', () => console.log('Client disconnected'))
   socket.on('message', message => console.log(`Client ${socket.id} message: ${message}`))
-  socket.on('message', message => socket.broadcast.emit('message', message))
+  socket.on('message', message => socket.broadcast.emit('message', {
+    body: message,
+    from: socket.id,
+  }))
 })
 
 // Express config
